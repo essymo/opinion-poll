a3c3c8c51b52e960ad40effd6d31ea0cea926ecb
essymo <esthermochari@gmail.com>
2014-04-22 16:36:43 +0300

    creating an opinion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Opinionpoll::Application.config.secret_key_base = '8ac5b4498e6392143fa10fb46cb2db1810188675b1a0e5d2b6278e43af5389be4e3a47d71fdd11cf6296e7c8d9bf54f9861a35dba911d02e98bf636f95bcb749'
